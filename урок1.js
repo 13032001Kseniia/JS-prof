e265b173cf6d5c78d51e16890caf6670e067d047
13032001Kseniia <86870984+13032001Kseniia@users.noreply.github.com>
2021-11-01 17:16:28 +0300

    Add files via upload

---
+++ урок1.js
@@ -0,0 +1,18 @@
+const products=[
+    {id:1,title:'Notebook',price:2000},
+    {id:2,title:'Mouse',price:20},
+    {id:3,title:'Keyboard',price:200},
+    {id:4,title:'Gamepad',price:50},
+];
+const renderProduct=(product,img='1img.jpg') =>{
+    return`<div class="product-item">
+  <img src="${img}">
+   <h3>${product.title}</h3>
+   <p>${product.price}</p>
+   <button class="buy-btn">Купить</button>
+   </div>`};
+  
+const renderPage=list =>{
+    document.querySelector('.products').innerHTML=list.map(product=>renderProduct(product)).join('');
+};
+renderPage(products);   
